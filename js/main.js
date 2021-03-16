0308d4f0e8551c51de383241d4e7dd2a8fafdabe
nstmld <an.moldavsky@gmail.com>
2021-03-17 01:46:14 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -14,67 +14,71 @@ const getRandomCoordinate = function (min, max, length) {
   return Number(coordinate);
 }
 
-const author = {
-  avatar: 'img/avatars/user/0' + getRandomInteger(0, 8) + '.png',
-};
+const createItem = () => {
+  const CHECK_TIME = [
+    '12:00',
+    '13:00',
+    '14:00',
+  ];
 
-author;
+  const FEATURES_LIST = [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner',
+  ];
 
-const CHECK_TIME = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
+  const TYPE_LIST = [
+    'palace',
+    'flat',
+    'house',
+    'bungalow',
+  ];
 
-const FEATURES_LIST = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-
-const TYPE_LIST = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-];
-
-const location = {
-  x: getRandomCoordinate(35.65000, 35.70000, 5),
-  y: getRandomCoordinate(139.70000, 139.80000, 5),
-};
-
-const createFeaturesList = () => {
-  const randomArrLength = getRandomInteger(0, FEATURES_LIST.length);
-  const randomStartElement = getRandomInteger(0, FEATURES_LIST.length);
-  const resultArr = FEATURES_LIST.splice(randomStartElement,randomArrLength);
-
-  return resultArr;
-};
-
-const createPhotosLinks = () => new Array(getRandomInteger(0,3)).fill('http://o0.github.io/assets/images/tokyo/hotel').map((value, index) => value += `${index + 1}.jpg`);
-
-const createOffer = () => {
   const randomCheckinTime = getRandomInteger(0, CHECK_TIME.length - 1);
   const randomCheckoutTime = getRandomInteger(0, CHECK_TIME.length - 1);
   const randomType = getRandomInteger(0, TYPE_LIST.length - 1);
+  const createPhotosLinks = () => new Array(getRandomInteger(0,3)).fill('http://o0.github.io/assets/images/tokyo/hotel').map((value, index) => value += `${index + 1}.jpg`);
 
-  return {
-    title: 'Временный заголовок',
-    address: location.x + ',' + location.y,
-    price: getRandomInteger(0, 10000),
-    type: TYPE_LIST[randomType],
-    rooms: getRandomInteger(1, 10),
-    guests: getRandomInteger(1, 20),
-    checkin: CHECK_TIME[randomCheckinTime],
-    checkout: CHECK_TIME[randomCheckoutTime],
-    features: createFeaturesList(),
-    description: 'Случайное описание помещения',
-    photos: createPhotosLinks(),
+  const createFeaturesList = () => {
+    const randomArrLength = getRandomInteger(0, FEATURES_LIST.length);
+    const randomStartElement = getRandomInteger(0, FEATURES_LIST.length);
+
+    return FEATURES_LIST.splice(randomStartElement,randomArrLength);
+  };
+
+  return  {
+    author: {
+      avatar: 'img/avatars/user/0' + getRandomInteger(0, 8) + '.png',
+    },
+
+    offer: {
+      title: 'Временный заголовок',
+      address: location.x + ',' + location.y,
+      price: getRandomInteger(0, 10000),
+      type: TYPE_LIST[randomType],
+      rooms: getRandomInteger(1, 10),
+      guests: getRandomInteger(1, 20),
+      checkin: CHECK_TIME[randomCheckinTime],
+      checkout: CHECK_TIME[randomCheckoutTime],
+      features: createFeaturesList(),
+      description: 'Случайное описание помещения',
+      photos: createPhotosLinks(),
+    },
+
+    location: {
+      x: getRandomCoordinate(35.65000, 35.70000, 5),
+      y: getRandomCoordinate(139.70000, 139.80000, 5),
+    },
   };
 }
 
-createOffer();
+const itemsCount = 10;
+
+const itemsList = (length = 0, item) => {
+  return Array.from({length: length}, item);
+}
+
+itemsList(itemsCount, createItem)
